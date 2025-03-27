96b3c59e12307dbed71a932ecf615bdcb8bfd477
Vanessaly100 <vanessalydia360@gmail.com>
2025-03-27 12:17:17 +0100

    Fix server port for Render

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.get("/", (_req,res) =>{
 
 
 //  Start Server
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
